9b89a61fac8c4c7a96f0868f9ee58cfa3b6a81f4
王彩霖 <root@Bluedot-Development-Cailin.localdomain>
2022-04-20 13:58:54 +0800

    feat: add migrations & change routes

---
+++ Dockerfile.dev
@@ -0,0 +1,12 @@
+FROM registry-vpc.cn-beijing.aliyuncs.com/bluedot/php:7.4
+
+WORKDIR /var/www/html
+
+COPY . /var/www/html
+
+COPY deploy/entrypoint.sh /usr/local/bin/entrypoint
+RUN chmod +x /usr/local/bin/entrypoint
+
+EXPOSE 80 9000
+
+ENTRYPOINT ["/usr/local/bin/entrypoint"]
---
+++ deploy/entrypoint.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+set -e
+
+rm /etc/nginx/sites-enabled/*
+cp deploy/nginx.conf /etc/nginx/conf.d/nginx.conf
+
+cp /usr/local/etc/php/php.ini-production /usr/local/etc/php/php.ini
+
+cd /var/www/html
+
+chown -R www-data:www-data storage && chmod -R 777 storage/
+chown -R www-data:www-data bootstrap/cache && chmod -R 777 bootstrap/cache
+
+echo "cron"
+cp deploy/crontab /etc/cron.d/
+chmod 0644 /etc/cron.d/crontab
+crontab  /etc/cron.d/crontab
+cron -f &
+
+echo "supervisor"
+cp deploy/supervisor.conf  /etc/supervisor/conf.d/
+supervisord -c /etc/supervisor/supervisord.conf
+supervisorctl update
+supervisorctl start laravel-queue:*
+
+echo 'cache'
+php artisan config:cache
+php artisan route:cache
+php artisan event:cache
+
+# 后台启动
+php-fpm -D && nginx -g 'daemon off;'
