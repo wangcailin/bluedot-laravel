1af49b2f08044ca2f3ace8768d692633ab43ede0
root <root@DESKTOP-RTJUT93>
2022-04-23 13:07:59 +0800

    feat: 权限初步测试完毕

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -2,11 +2,9 @@
 
 namespace App\Providers;
 
-use Illuminate\Cache\RateLimiting\Limit;
 use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\RateLimiter;
 use Illuminate\Support\Facades\Route;
+use Composer\Composer;
 
 class RouteServiceProvider extends ServiceProvider
 {
@@ -26,7 +24,9 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string|null
      */
-    // protected $namespace = 'App\\Http\\Controllers';
+    protected $namespace = 'App\\Http\\Controllers';
+
+    protected $prefix = 'api';
 
     /**
      * Define your route model bindings, pattern filters, etc.
@@ -35,29 +35,18 @@ class RouteServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        $this->configureRateLimiting();
-
+        if (config('app.env') !== 'production') {
+            $this->prefix = 'api-dev';
+        }
         $this->routes(function () {
-            Route::prefix('api')
+            Route::prefix($this->prefix)
                 ->middleware('api')
                 ->namespace($this->namespace)
                 ->group(base_path('routes/api.php'));
-
-            Route::middleware('web')
-                ->namespace($this->namespace)
-                ->group(base_path('routes/web.php'));
         });
-    }
 
-    /**
-     * Configure the rate limiters for the application.
-     *
-     * @return void
-     */
-    protected function configureRateLimiting()
-    {
-        RateLimiter::for('api', function (Request $request) {
-            return Limit::perMinute(60)->by(optional($request->user())->id ?: $request->ip());
-        });
+        Composer::routes([], [
+            'prefix' => $this->prefix,
+        ]);
     }
 }
