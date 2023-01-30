29bbaf4017a7a97764144d4234027316490e5b09
王彩霖 <root@Bluedot-Development-Cailin>
2023-01-30 17:09:19 +0800

    feat(System)：升级PHP8.1

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,13 +1,22 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Laravel\Passport\ClientRepository;
 use Composer\Support\Auth\Models\User;
 
 class DatabaseSeeder extends Seeder
 {
-
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
     public function run()
     {
         User::createAdminUser('bluedot', 'Bluedot@2023');
+
+        $client = new ClientRepository();
+        $client->createPasswordGrantClient(null, 'Default Tenant Client', '');
+        $client->createPersonalAccessClient(null, 'Default Tenant Personal Client', '');
     }
 }
